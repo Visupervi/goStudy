42e0288d40bbfd3b753bab5b91ed354efb45ee27
Visupervi <Visuper@outlook.com>
2023-03-20 14:01:59 +0800

    feat: go学习第六天 自定义模块

---
+++ src/day06/main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"day06/utils"
+	"fmt"
+)
+
+func main() {
+	//  主要学些go的语法
+
+	//	顺序控制
+	//	分支控制
+	//	if else
+	//	if 条件表达式 {}
+	//	if else if else
+	//	循环控制
+
+	//var age byte
+	//fmt.Println("请输入你的年龄")
+	//fmt.Scanln(&age)
+	//if age > 18 {
+	var sum float32 = utils.Cal(11.2, 11.1)
+	//	fmt.Println("你已经大于18，请为你的行为负责")
+	//} else {
+	fmt.Println("请在监护人的陪同下观看", sum)
+	//}
+	//	go的每一个文件就是一个包
+
+}
---
+++ src/day06/utils/utils.go
@@ -0,0 +1,5 @@
+package utils
+
+func Cal(n1 float32, n2 float32) float32 {
+	return n2 + n1
+}
