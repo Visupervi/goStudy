1b0cee71b1d9c90bb19a1357f162bc231df1d4b4
Visupervi <Visuper@outlook.com>
2024-04-23 19:11:24 +0800

    feat: go 学习第34天，redis(一)

---
+++ src/day34-1/main.go
@@ -0,0 +1,26 @@
+package main
+
+// tcp
+
+// 用户注册 用户登陆 显示在线列表 群聊 点对点聊天 离线留言
+
+// redis学习 mysql oracle
+// 如何在golang中使用redis
+// redis的使用
+// noSql数据库
+// redis, remote dictionary server （远程字典服务器）， redis性能非常高，单机能够达到15w qps
+// 通常适合做缓存， 也可以持久化
+// 分布式内存数据库，基于内存运行
+
+// redis的基本使用
+// redis五大数据类型
+// string, hash, list列表 set集合，zset 有序集合
+// redis的字符串最大512M
+// setex key seconds value 设置超时间
+
+// redis哈希是一个string类型的field和value的映射表。
+func main() {
+	//server.Start()
+
+	//	添加key-val [set]
+}
